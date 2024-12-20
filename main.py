33531026dd7690ebba2074079be6485612a8b1e7
Paul MacCready <polocahrbel@gmail.com>
2024-12-20 14:40:11 +0100

    Key and interface fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from interface import Main
 def main(): 
     folder = aes.read_folder("./dossier_confidentiel")
     aes.encrypt_folder(folder, aes.key)
-    Main()
+    Main(aes.key)
 
 
 if __name__ == "__main__":
